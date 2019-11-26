6cefedb4f432046c4527fd961621fbe796b1eb1e
Matthew Crawford <crawfordm2000@gmail.com>
2019-11-26 14:45:01 -0500

    added doctors

--- src/Doctors.js
+++ src/Doctors.js
@@ -13,9 +13,8 @@ class Doctors extends React.Component{
         doctors: [],
         newDoctor: {
             name: "",
-            age: null,
-            gender: "",
-            doctorId: null
+            address: "",
+            phone: null,
         }
     }
     componentDidMount() {
@@ -32,7 +31,7 @@ class Doctors extends React.Component{
         console.log(response);
       });
     };
-    creatDoctor = e => {
+    createDoctor = e => {
       e.preventDefault();
       axios({
         url: `${doctorUrl}/doctors`,
@@ -40,7 +39,7 @@ class Doctors extends React.Component{
         data: { newDoctor: this.state.newDoctor }
       }).then(response => {
         //   this.setState(prevState => ({
-        //     doctors: [...prevState.doctors, response.data.doctors]
+        //     doctors: [...prevState.doctsors, response.data.doctors]
         //   }));
         this.setState({ doctors: response.data.doctors });
       });
@@ -48,11 +47,11 @@ class Doctors extends React.Component{
     handleChange = e => {
       let newDoctor = {
         [e.target.name]: e.target.value,
-        [e.target.age]: e.target.value,
-        [e.target.gender]: e.target.value
+        [e.target.address]: e.target.value,
+        [e.target.phone]: e.target.value
       };
       this.setState((prevState, currentState) => ({
-        newPatient: { ...prevState.newDoctor, ...newDoctor }
+        newDoctor: { ...prevState.newDoctor, ...newDoctor }
       }));
     };
     deleteDoctor = e => {
@@ -67,39 +66,31 @@ class Doctors extends React.Component{
         console.log(this.state);
         const doctorEls = this.state.doctors.map(doctor => {
           return (
-              
-                  <div key={doctor.id} className="doctorsCardDiv">
-                      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB-ckJyRnhYpswKfyGR1aLAPiLFx3eh5yUk_P1fh7dpeZ8SHBW&s" /> */}
-                      <p>Dr.{doctor.name}</p>
-                      <p>Address: {doctor.address}</p>
-                      <p>Phone: {doctor.phone}</p>
-                      <p>Id: {doctor.id}</p>
-                      <i class="material-icons md-dark" onClick={this.deleteDoctor}>delete</i>
+                  <div>
+                    <div key={doctor.id} className="doctorsCardDiv">
+                        {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB-ckJyRnhYpswKfyGR1aLAPiLFx3eh5yUk_P1fh7dpeZ8SHBW&s" /> */}
+                        <p>Dr.{doctor.name}</p>
+                        <p>Address: {doctor.address}</p>
+                        <p>Phone: {doctor.phone}</p>
+                        <p>Id: {doctor.id}</p>
+                        <i class="material-icons md-dark" onClick={this.deleteDoctor}>delete</i>
+                    </div>
+            
                   </div>
              
           );
         });
         return(
             <div id="doctorsDiv">
-              {/* This is the form that adds a doctor  */}
-              {/* <div id="doctorFormModal">
-              <form
-              onSubmit={this.createPatient}
-              onChange={e => this.handleChange(e)}
-              >
-              Name: <input type="text" name="name" />
-              Age: <input type="number" name="age" />
-              {/* Gender: <input type="text" name="gender" /> */}
-              {/* Gender:
-              <select id="genderDropdown" name="gender">
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
-              </select>
-              <input type="submit" value="Add New Patient" />
-              </form> */}
-              {/* </div>  */}
-              <i class="material-icons md-dark" id="addButton">add_circle</i>
+              
+              
+              <h1>Create Doctor Profile</h1>
+                    <form onSubmit={this.createDoctor} onChange={e => this.handleChange(e)}>
+                      Name: <input type="text" name="name" />
+                      Address: <input type="text" name="address" />
+                      Phone: <input type="number" name="phone" />
+                      <input type="submit" value="New Doctor Submit" />
+                    </form>
               <ul>{doctorEls}</ul>
             </div>
         )
